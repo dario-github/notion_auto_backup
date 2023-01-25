003d7ea21cb30a4a442a5f4f0cf98807dc4bf051
dario-github <zdclink@gmail.com>
2023-01-25 06:45:07 +0000

    [FEATURE] 文件更新方式由upload改为update

--- pipline.sh
+++ pipline.sh
@@ -1,9 +1,10 @@
 MYDIR=`dirname $0`
 cd $MYDIR
 export PYTHONPATH=$MYDIR
-GFOLDID='1l-VJt0Yj8rZDmZqY1ZED0isuBj3A1rxv'
+# GFOLDID='1l-VJt0Yj8rZDmZqY1ZED0isuBj3A1rxv'
+GFILEID='1z9D9S7-lNlvajur1TLOPvR0dC3rn5bLM'
 
 /usr/bin/python3.8 -m poetry run python main.py --config .config_file.json && \
 mv build/*.zip .notion_backup/notion_backup_lastest.zip && \
-/root/go/bin/gdrive files upload --parent $GFOLDID .notion_backup && \
+/root/go/bin/gdrive files update $GFILEID .notion_backup/notion_backup_lastest.zip && \
 rm -rf build/*
