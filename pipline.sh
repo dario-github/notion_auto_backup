b33eb25f65b3ea10d9e20e90760aedf0d506df52
dario-github <zdclink@gmail.com>
2023-01-25 02:54:21 +0000

    [FEATURE] gdrive版本更新到3.6，修改上传文件代码

--- pipline.sh
+++ pipline.sh
@@ -5,5 +5,5 @@ GFOLDID='1l-VJt0Yj8rZDmZqY1ZED0isuBj3A1rxv'
 
 /usr/bin/python3.8 -m poetry run python main.py --config .config_file.json && \
 mv build/*.zip .notion_backup/notion_backup_lastest.zip && \
-/root/go/bin/gdrive sync upload .notion_backup $GFOLDID && \
+/root/go/bin/gdrive files upload --parent $GFOLDID .notion_backup && \
 rm -rf build/*
